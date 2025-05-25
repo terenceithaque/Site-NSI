e2e87a7a4a8b9b87440f2e6f31e604a820693515
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-05-25 11:39:46 +0200

    Ajouté getAnswers()

--- JS/reponses_quizz.js
+++ JS/reponses_quizz.js
@@ -4,3 +4,8 @@
 // Bonnes réponses
 let bonnesReponses = ["rock_and_roll", "1956", "new_york", "version", "cars"];
 
+// Fonction pour récupérer les réponses
+function getAnswers(){
+    answers = sessionStorage.getItem("answers");
+    return answers;
+}
